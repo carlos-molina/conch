1b44b8b1726705d9e4c5c84fbd53119b3b26097d
Giannis Sfyrakis <giannisfyrakis@gmail.com>
2013-03-14 18:45:10 +0200

    - added custom listeners for Agenda and Working Memory

---
+++ CCCRest-ear/CCCRest-ear-commons/src/main/java/uk/ac/ncl/util/CustomWorkingMemoryEventListener.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package uk.ac.ncl.util;
+
+import org.drools.event.rule.ObjectInsertedEvent;
+import org.drools.event.rule.ObjectRetractedEvent;
+import org.drools.event.rule.ObjectUpdatedEvent;
+import org.drools.event.rule.WorkingMemoryEventListener;
+
+import java.util.logging.Logger;
+
+public class CustomWorkingMemoryEventListener implements WorkingMemoryEventListener {
+	
+	private final static Logger logger = Logger.getLogger(CustomWorkingMemoryEventListener.class.toString());
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.drools.event.rule.WorkingMemoryEventListener#objectInserted(org.drools
+	 * .event.rule.ObjectInsertedEvent)
+	 */
+	@Override
+	public void objectInserted(ObjectInsertedEvent event) {
+		logger.info("Object Inserted: " + event.getFactHandle() + " Knowledge Runtime: " + event.getKnowledgeRuntime());
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.drools.event.rule.WorkingMemoryEventListener#objectRetracted(org.
+	 * drools.event.rule.ObjectRetractedEvent)
+	 */
+	@Override
+	public void objectRetracted(ObjectRetractedEvent event) {
+		logger.info("Object Retracted: " + event.getFactHandle() + " Knowledge Runtime: " + event.getKnowledgeRuntime());
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.drools.event.rule.WorkingMemoryEventListener#objectUpdated(org.drools
+	 * .event.rule.ObjectUpdatedEvent)
+	 */
+	@Override
+	public void objectUpdated(ObjectUpdatedEvent event) {
+		logger.info("Object Updated: " + event.getFactHandle() + " Knowledge Runtime: " + event.getKnowledgeRuntime());
+
+	}
+}
