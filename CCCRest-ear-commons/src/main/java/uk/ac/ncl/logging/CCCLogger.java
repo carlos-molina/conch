7dc8bfd7d0f41157e0c23df5b173c57c4fdb2dfa
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2014-12-03 00:01:48 +0000

    added file logger

--- CCCRest-ear-commons/src/main/java/uk/ac/ncl/logging/CCCLogger.java
+++ CCCRest-ear-commons/src/main/java/uk/ac/ncl/logging/CCCLogger.java
@@ -1,7 +1,70 @@
 package uk.ac.ncl.logging;
 
+import org.jboss.logging.Logger;
+
 /**
  * Created by alpac on 02/12/2014.
  */
 public class CCCLogger {
+
+    // a JBoss Logging 3 style level (FATAL, ERROR, WARN, INFO, DEBUG, TRACE), or a special level (OFF, ALL).
+    private static Logger log = Logger.getLogger(CCCLogger.class.getName());
+    static
+    {
+        logFatal("");
+        logError("");
+        logWarn("");
+        logInfo("");
+        logDebug("");
+        logTrace("");
+    }
+
+    public static void logFatal(String msg)
+    {
+        if (log.isEnabled(Logger.Level.FATAL))
+        {
+            log.fatal(msg);
+        }
+    }
+
+    public static void logError(String msg)
+    {
+        if (log.isEnabled(Logger.Level.ERROR))
+        {
+            log.error(msg);
+        }
+    }
+
+    public static void logWarn(String msg)
+    {
+        if (log.isEnabled(Logger.Level.WARN))
+        {
+            log.warn(msg);
+        }
+    }
+
+    public static void logInfo(String msg)
+    {
+        if (log.isEnabled(Logger.Level.INFO))
+        {
+            log.info(msg);
+        }
+    }
+
+    public static void logDebug(String msg)
+    {
+        if (log.isEnabled(Logger.Level.DEBUG))
+        {
+            log.debug(msg);
+        }
+    }
+
+    public static void logTrace(String msg)
+    {
+        if (log.isEnabled(Logger.Level.TRACE))
+        {
+            log.trace(msg);
+        }
+    }
 }
+
