0df0e6494bce0212f1fd719f3f8975d6528b5bf0
gsfyrakis <giannisfyrakis@gmail.com>
2015-07-31 18:55:59 +0100

    updated runClient shellscript

--- CCCRestClient/runClient.sh
+++ CCCRestClient/runClient.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # 1. assemble jar with dependencies 
 # 2. execute jar with sequences
-mvn clean compile assembly:single  && 
+mvn clean package assembly:single  && 
 java -jar target/CCCRestClient-1-jar-with-dependencies.jar /Users/alpac/Documents/JAVA-PROJECTS/conch/CCCRestClient/ExecSequencesSamples/ccTestSeq-xml/
