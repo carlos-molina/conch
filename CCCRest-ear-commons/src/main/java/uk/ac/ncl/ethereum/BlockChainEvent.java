2591fc332507cff6dbb9b4a9e7c7c5e99b2ad925
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-06 00:32:41 +0100

    use properties file

--- CCCRest-ear-commons/src/main/java/uk/ac/ncl/ethereum/BlockChainEvent.java
+++ CCCRest-ear-commons/src/main/java/uk/ac/ncl/ethereum/BlockChainEvent.java
@@ -1,20 +1,15 @@
 package uk.ac.ncl.ethereum;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import org.web3j.crypto.CipherException;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.RawTransaction;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.tx.Transfer;
-import org.web3j.utils.Convert;
 import uk.ac.ncl.logging.CCCLogger;
 
 /**
@@ -27,13 +22,16 @@ public class BlockChainEvent {
   private TransactionReceipt transactionReceipt;
   private BigInteger nonce;
   private RawTransaction rawTransaction;
+  private String keyStorePath;
+  private String walletPSW;
 
   public BlockChainEvent() throws IOException {
     CCCLogger.logInfo("new blockchain event");
-
   }
 
-  public Web3j initWeb3() {
+  public Web3j initWeb3(String keyStorePath, String walletPSW) {
+    this.keyStorePath = keyStorePath;
+    this.walletPSW = walletPSW;
     return web3 = Web3j.build(new HttpService());  // http://localhost:8545/
   }
 
@@ -41,33 +39,10 @@ public class BlockChainEvent {
     web3ClientVersion = web3.web3ClientVersion().send();
     String clientVersion = web3ClientVersion.getWeb3ClientVersion();
     return clientVersion;
-
   }
 
   public Credentials getCredentials() throws IOException, CipherException {
-    // TODO add password and keystore path for ethereum wallet
-    String keyStorePath = "path/to/keystore";
-    String walletPsw = "password";
-
-    return WalletUtils.loadCredentials(walletPsw, keyStorePath);
+    return WalletUtils.loadCredentials(walletPSW, keyStorePath);
   }
-
-  public TransactionReceipt sendEther(String address) throws Exception {
-
-    return transactionReceipt = Transfer.sendFunds(
-        web3, getCredentials(), address,
-        BigDecimal.valueOf(0.01), Convert.Unit.ETHER)
-        .send();
-  }
-
-
-  //custom raw transaction
-  public BigInteger getNextAvailableNonce(String address) throws IOException {
-    EthGetTransactionCount ethGetTransactionCount = web3.ethGetTransactionCount(
-        address, DefaultBlockParameterName.LATEST).send();
-    return nonce = ethGetTransactionCount.getTransactionCount();
-
-  }
-
 
 }
